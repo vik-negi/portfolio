a562e5863fa2d3bf65553be9a7d91357384fe1b9
vik-negi <vikramnegi175@gmail.com>
2025-04-17 20:15:40 +0530

    shift code to next.js new1

--- src/script/commonScript.js
+++ src/script/commonScript.js
@@ -6,16 +6,16 @@
 
 //HEADER ANIMATION FUNCTION
 
-const anchors = header.querySelectorAll("a");
+// const anchors = header.querySelectorAll("a");
 
-const headerAnim = () => {
-  const offsetY = window.scrollY;
-  console.log("callled");
+// const headerAnim = () => {
+//   const offsetY = window.scrollY;
+//   console.log("callled");
 
-  header.classList.contains("header-scroll-style")
-    ? offsetY < 50 && header.classList.remove("header-scroll-style")
-    : offsetY > 200 && header.classList.add("header-scroll-style");
-};
+//   header.classList.contains("header-scroll-style")
+//     ? offsetY < 50 && header.classList.remove("header-scroll-style")
+//     : offsetY > 200 && header.classList.add("header-scroll-style");
+// };
 
-headerAnim();
-window.addEventListener("scroll", headerAnim);
+// headerAnim();
+// window.addEventListener("scroll", headerAnim);
